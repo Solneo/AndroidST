f0bac196849e934bbe027c467a393b052bb827eb
Solneo <chdanilr@gmail.com>
2018-10-01 19:48:28 +0300

    four clever, zxing its work

--- app/src/main/java/com/example/myapplicationst/QrReader/QRReaderActivity.java
+++ app/src/main/java/com/example/myapplicationst/QrReader/QRReaderActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplicationst.QrReader;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,15 +13,19 @@ import android.hardware.Camera.AutoFocusCallback;
 import android.hardware.Camera.Size;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.hardware.camera2.*;
 
 /* Import ZBar Class files */
 import com.example.myapplicationst.R;
-
+import com.google.android.gms.common.logging.Logger;
+import com.google.zxing.Result;
+import android.provider.SyncStateContract.Constants;
 import net.sourceforge.zbar.ImageScanner;
 import net.sourceforge.zbar.Image;
 import net.sourceforge.zbar.Symbol;
@@ -31,31 +36,54 @@ import net.sourceforge.zbar.Config;
 import net.sourceforge.zbar.ImageScanner;
 import net.sourceforge.zbar.Image;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 
 /**
  * Created by Ыщвф on 30.09.2018.
  */
 
-public class QRReaderActivity extends Activity {
-    private static final String cameraPerm = Manifest.permission.CAMERA;
-    private Camera mCamera;
-   // private CameraPreview mPreview;
-    private Handler autoFocusHandler;
-    private FrameLayout preview;
-    private TextView scanText;
-    private ImageView bar_code;
-    private EditText code_for_bar;
-    private ImageScanner scanner;
-    private boolean barcodeScanned = false;
-    private boolean previewing = true;
-    private String lastScannedCode;
-    private Image codeImage;
+public class QRReaderActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.qrreader_layout);
+        private ZXingScannerView mScannerView;
 
+        @Override
+        public void onCreate(Bundle state) {
+            super.onCreate(state);
+            // Programmatically initialize the scanner view
+            mScannerView = new ZXingScannerView(this);
+            // Set the scanner view as the content view
+            setContentView(mScannerView);
+        }
 
+        @Override
+        public void onResume() {
+            super.onResume();
+            // Register ourselves as a handler for scan results.
+            mScannerView.setResultHandler(this);
+            // Start camera on resume
+            mScannerView.startCamera();
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            // Stop camera on pause
+            mScannerView.stopCamera();
+        }
+
+        @Override
+        public void handleResult(Result rawResult) {
+            // Do something with the result here
+            // Prints scan results
+           // Logger.verbose("result", rawResult.getText());
+            // Prints the scan format (qrcode, pdf417 etc.)
+           // Logger.verbose("result", rawResult.getBarcodeFormat().toString());
+            //If you would like to resume scanning, call this method below:
+            //mScannerView.resumeCameraPreview(this);
+            Intent intent = new Intent();
+           // intent.putExtra(AppConstants.KEY_QR_CODE, rawResult.getText());
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
-}

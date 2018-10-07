2de0d36d97709f358e256c52a0ad31c01ae7c90a
Solneo <chdanilr@gmail.com>
2018-10-07 10:14:16 +0300

    work with retrofit (for JSON) started

--- app/src/main/java/com/example/myapplicationst/OneObjekt.java
+++ app/src/main/java/com/example/myapplicationst/OneObjekt.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.example.myapplicationst.NetCommunication.AdapterResponse;
 import com.example.myapplicationst.NetCommunication.PostModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -19,10 +23,14 @@ import retrofit2.Response;
  */
 
 public class OneObjekt extends Activity {
+    RecyclerView recyclerView;
+    List<PostModel> posts;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.one_objekt_layout);
+        startResponse();
     }
 
     public void onBackPressedButton(View v) {
@@ -31,6 +39,14 @@ public class OneObjekt extends Activity {
     }
 
     public void startResponse() {
+        posts = new ArrayList<>();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        AdapterResponse adapter = new AdapterResponse(posts);
+        recyclerView.setAdapter(adapter);
         try {
             Response response = AppNetCom.getApi().getData("user").execute();
         }
